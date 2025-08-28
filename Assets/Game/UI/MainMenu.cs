b23ee7b3740a9044ea9c977ec5a99a67dd52da0b
Yang Yang <yangduakali@gmail.com>
2025-08-28 21:28:03 +0700

    init

---
+++ Assets/Game/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace IdleGear.UI
+{
+    [UxmlElement]
+    public partial class MainMenu : VisualElement
+    {
+        public MainMenu()
+        {
+            var buttonHome = new Button();
+            var buttonQuest = new Button();
+            var buttonBattle = new Button();
+            
+            
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Core/IdleGearManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace IdleGear
+{
+    public class IdleGearManager : MonoBehaviour
+    {
+        
+    }
+}
