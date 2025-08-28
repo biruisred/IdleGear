f81de1ba0d6cae93fa99eb2a5e3fddca93989983
Yang Yang <yangduakali@gmail.com>
2025-08-29 05:45:35 +0700

    remove config

--- Assets/Scripts/Core/IdleGearManager.cs
+++ Assets/Scripts/Core/IdleGearManager.cs
@@ -10,23 +10,23 @@ namespace IdleGear
     {
         private async void Start()
         {
-            // try
-            // {
-            //     var status = await FirebaseApp.CheckAndFixDependenciesAsync();
-            //     if (status != DependencyStatus.Available)
-            //     {
-            //         Debug.Log($"Could not resolve all Firebase databases dependencies: {status}");
-            //         return;
-            //     }
-            //
-            //     var auth = FirebaseAuth.DefaultInstance;
-            //     await auth.SignInAnonymouslyAsync();
-            //
-            // }
-            // catch (Exception e)
-            // {
-            //     Debug.LogException(e);
-            // }
+            try
+            {
+                var status = await FirebaseApp.CheckAndFixDependenciesAsync();
+                if (status != DependencyStatus.Available)
+                {
+                    Debug.Log($"Could not resolve all Firebase databases dependencies: {status}");
+                    return;
+                }
+
+                var auth = FirebaseAuth.DefaultInstance;
+                await auth.SignInAnonymouslyAsync();
+
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
         }
     }
 }
